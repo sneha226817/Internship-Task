f5212247cd62c996d92e080db91a238422a1d433
Sneha <sonkamblesneha18@gmail.com>
2026-07-27 12:00:57 +0530

    data.js

--- js/data.js
+++ js/data.js
@@ -2,93 +2,50 @@ const restaurants = [
 
 {
 id:1,
-
 name:"Pizza World",
-
 rating:4.8,
-
 cuisine:"Italian",
-
 image:"images/pizza.jpg"
-
 },
 
 {
 id:2,
-
 name:"Burger King",
-
 rating:4.5,
-
 cuisine:"Fast Food",
-
 image:"images/burger.jpg"
-
 },
 
 {
 id:3,
-
 name:"Sushi House",
-
 rating:4.9,
-
 cuisine:"Japanese",
-
 image:"images/sushi.jpg"
-
-}
-
-];
-
-const menu=[
-
-{
-
-id:101,
-
-restaurant:1,
-
-name:"Margherita Pizza",
-
-price:12,
-
-veg:true,
-
-image:"images/m1.jpg"
-
 },
 
 {
-
-id:102,
-
-restaurant:1,
-
-name:"Farmhouse Pizza",
-
-price:15,
-
-veg:true,
-
-image:"images/m2.jpg"
-
+id:4,
+name:"Royal Biryani",
+rating:4.7,
+cuisine:"Indian",
+image:"images/biryani.jpg"
 },
 
 {
+id:5,
+name:"Taco Fiesta",
+rating:4.4,
+cuisine:"Mexican",
+image:"images/taco.jpg"
+},
 
-id:103,
-
-restaurant:2,
-
-name:"Cheese Burger",
-
-price:8,
-
-veg:false,
-
-image:"images/m3.jpg"
-
+{
+id:6,
+name:"Healthy Bowl",
+rating:4.6,
+cuisine:"Healthy",
+image:"images/salad.jpg"
 }
 
 ];
